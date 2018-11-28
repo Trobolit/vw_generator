783b6e1e143ced4591994819be61326bd031ef4e
Robert Hedman <robert.hedman@mac.com>
2018-11-28 08:52:18 +0100

    added delta pose estimator

--- src/vw_generator_node.cpp
+++ src/vw_generator_node.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/Float32MultiArray.h"
-
+#include <math.h>
 
 #define NODE_NAME "vw_generator"
 #define SUBSCRIBE_WHEEL_VEL "wheel_velocity"
@@ -14,6 +14,7 @@
 
 
 ros::Publisher vw_estimator;
+ros::Publisher d_pose;
 ros::Subscriber wheel_vel_sub;
 
 std_msgs::Float32MultiArray vw_estimate_msg;
@@ -24,6 +25,8 @@ float current_R_vel = 0;
 float vhat = 0;
 float what = 0;
 
+
+void deltaPose();
 
 void encoderCallback(const std_msgs::Float32MultiArray::ConstPtr& array)
 {
@@ -38,6 +41,22 @@ void encoderCallback(const std_msgs::Float32MultiArray::ConstPtr& array)
 
 	vw_estimator.publish(vw_estimate_msg);    // publish data
 	vw_estimate_msg.data.clear();
+	deltaPose();
+}
+
+void deltaPose()
+{
+	geometry_msgs::Twist dp;
+
+ 	dp.linear.x = vhat * sin(what / LOOP_FREQ);
+	dp.linear.y = vhat * cos(what / LOOP_FREQ);
+	dp.linear.z = 0;
+
+	dp.angular.x = 0;
+	dp.angular.y = 0;
+	dp.angular.z = what;
+
+	d_pose.publish(dp);
 }
 
 
@@ -47,6 +66,7 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh;
 	
 	vw_estimator = nh.advertise<std_msgs::Float32MultiArray>(ADVERTISE_VW_ESTIMATE, BUFFER_SIZE);
+	d_pose = nh.advertise<geometry_msgs::Twist>("d_pose", 1);
 	wheel_vel_sub = nh.subscribe<std_msgs::Float32MultiArray>(SUBSCRIBE_WHEEL_VEL, BUFFER_SIZE, encoderCallback);
 
 	ros::spin();
